f866a63b785f47091d9c1b87369209213b8ade53
Tyler Hemmie <tylerhemmie@Tylers-MacBook-Pro.local>
2014-03-20 13:51:55 -0700

    Initial Commit

---
+++ Checklists/ChecklistsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ChecklistsAppDelegate.h
+//  Checklists
+//
+//  Created by Tyler Hemmie on 3/20/14.
+//  Copyright (c) 2014 Tyler Hemmie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChecklistsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Checklists/ChecklistsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChecklistsViewController.h
+//  Checklists
+//
+//  Created by Tyler Hemmie on 3/20/14.
+//  Copyright (c) 2014 Tyler Hemmie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChecklistsViewController : UIViewController
+
+@end
