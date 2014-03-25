1dfcca9e3a177d0c5a08dfb16bf23ae98105efe5
Tyler Hemmie <tylerhemmie@Tylers-MacBook-Pro.local>
2014-03-25 16:33:30 -0700

    I actually added rows rather than cells in the last post. (just a clarification) In this I changed the for row method to create specific rows to be seen on the table view.

--- Checklists/ChecklistsViewController.m
+++ Checklists/ChecklistsViewController.m
@@ -31,6 +31,13 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView
-                             dequeueReusableCellWithIdentifier:@"ChecklistItem"]; return cell;
-}
+                             dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    UILabel *label = (UILabel *)[cell viewWithTag:1000];
+    if (indexPath.row == 0) { label.text = @"Walk the dog";
+    } else if (indexPath.row == 1) { label.text = @"Brush my teeth";
+    } else if (indexPath.row == 2) { label.text = @"Learn iOS development";
+    } else if (indexPath.row == 3) { label.text = @"Soccer practice";
+    } else if (indexPath.row == 4) { label.text = @"Eat ice cream";
+    }
+    return cell; }
 @end
